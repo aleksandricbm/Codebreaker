1ec73e9813946bf2d151db2d2c193a7389a0c4dc
Aleksander <aleksander.ignatkin@gmail.com>
2017-06-19 16:59:52 +0300

    fix guard clause

--- lib/codebreaker/ui.rb
+++ lib/codebreaker/ui.rb
@@ -7,20 +7,17 @@ class UI
   def enter_name
     puts "Dear friend,\n Please, puts your name:\n"
     @user_name = gets.chomp
-    if @user_name.empty?
-      puts "\nerror user name\n\n"
-      enter_name
-    end
+    return unless @user_name.empty?
+    puts "\nerror user name\n\n"
+    enter_name
   end
 
   def enter_number
     puts "Please, enter 4 number for 1 to 6:\n"
     @user_number = gets.chomp
-
-    unless @user_number[/^[1-6]{4}/]
-      puts "\nerror input type format\n"
-      enter_number
-    end
+    return @user_number if @user_number[/^[1-6]{4}/]
+    puts "\nerror input type format\n"
+    enter_number
   end
 
   def game_result(result)
